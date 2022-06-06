817ef3cdc1cd3a4b43527da8d5e7a2f65e432127
MattheWW1377 <105381653+MattheWW1377@users.noreply.github.com>
2022-06-06 23:55:02 +0500

    Add files via upload

---
+++ Classes/Bonus.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PigJump.Classes
+{
+    public class Bonus
+    {
+
+        public Physics physics;
+        public Image sprite;
+        public int type;
+
+        public Bonus(PointF pos, int type)
+        {
+            switch (type)
+            {
+                case 1:
+                    sprite = Properties.Resources.spring;
+                    physics = new Physics(pos, new Size(20, 20));
+                    break;
+                case 2:
+                    sprite = Properties.Resources.jetpack;
+                    physics = new Physics(pos, new Size(30, 30));
+                    break;
+            }
+            this.type = type;
+        }
+
+        public void DrawSprite(Graphics g)
+        {
+            g.DrawImage(sprite, physics.transform.position.X, physics.transform.position.Y, physics.transform.size.Width, physics.transform.size.Height);
+        }
+    }
+}
---
+++ Classes/Bullet.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PigJump.Classes
+{
+    public class Bullet
+    {
+        public Physics physics;
+        public Image sprite;
+
+        public Bullet(PointF pos)
+        {
+            sprite = Properties.Resources.bullet;
+            physics = new Physics(pos, new Size(-25, 20));
+        }
+
+        public void MoveUp()
+        {
+            physics.transform.position.Y -= 40;
+        }
+
+        public void DrawSprite(Graphics g)
+        {
+            g.DrawImage(sprite, physics.transform.position.X, physics.transform.position.Y, physics.transform.size.Width, physics.transform.size.Height);
+        }
+    }
+}
